27afb01d467f4cc7c2a7f306a08afbe51053dfe2
Lalo <aalvarezvbretana>
2020-05-09 17:15:50 +0200

    acabado

---
+++ src/tarefa2/Tarefa2.java
@@ -0,0 +1,20 @@
+
+package tarefa2;
+
+/**
+ *
+ * @author Lalo
+ */
+public class Tarefa2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Ventana ven = new Ventana();
+    ven.setVisible(true);
+    Ventana2 ven2 = new Ventana2();
+    ven2.setVisible(true);
+    }
+    
+}
